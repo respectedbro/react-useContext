d9590e34f916c8df554e4c49dc7f12540dffc3c6
NikDm <respectedoff@yandex.ru>
2025-09-03 14:09:17 +0300

    dyn

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,26 +1,43 @@
 import { Link, useSearchParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Posts = () => {
-  const postList = [
-    { id: 1, title: "Первый пост" },
-    { id: 2, title: "React круче Angular" },
-    { id: 3, title: "Фронтенд в 2025" },
-  ];
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const [searchParams] = useSearchParams();
   const filter = searchParams.get("filter");
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => {
+        setPosts(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error("Ошибка загрузки", err);
+        setLoading(false);
+      });
+  }, []);
 
   return (
     <>
       <h3>Posts</h3>
       {filter && <p>Фильтр: {filter}</p>}
-      <ul>
-        {postList.map((post) => (
-          <li key={post.id}>
-            <Link to={`/posts/${post.id}`}>{post.title}</Link>
-          </li>
-        ))}
-      </ul>
+      {loading ? (
+        <p> Loading...</p>
+      ) : (
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>
+              <Link to={`/posts/${post.id}`}>{post.title}</Link>
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 };

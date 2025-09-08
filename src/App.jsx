ee554fd41830f902a0e49d0a656e1d9df5aebefd
resAsus <respectedbro@yandex.ru>
2025-09-08 16:28:44 +0300

    hw

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import "./App.css";
-import { useDispatch, useSelector } from "react-redux";
-import { decrement, increment } from "./features/counter/counterSlice.js";
+import './App.css';
+import {useDispatch, useSelector} from 'react-redux';
+import {decrement, increment} from './features/counter/counterSlice.js';
+import Users from './compnents/Users/index.jsx';
 
 function App() {
-  const count = useSelector((state) => state.counter.value);
-  const dispatch = useDispatch();
+    const count = useSelector((state) => state.counter.value);
+    const dispatch = useDispatch();
 
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>Счётчик: {count}</h1>
-      <button onClick={() => dispatch(decrement())}>-</button>
-      <button onClick={() => dispatch(increment())}>+</button>
-    </div>
-  );
+    return (
+        <>
+            <div style={{padding: 20}}>
+                <h1>Счётчик: {count}</h1>
+                <button onClick={() => dispatch(decrement())}>-</button>
+                <button onClick={() => dispatch(increment())}>+</button>
+            </div>
+            <Users/>
+        </>
+    );
 }
 
 export default App;

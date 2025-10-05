1863ead4187f352efe158a9f545258b172fd0615
NikDm <respectedoff@yandex.ru>
2025-10-05 20:13:56 +0300

    suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,15 @@
-import './App.css';
-import {useDispatch, useSelector} from 'react-redux';
-import {decrement, increment} from './features/counter/counterSlice.js';
-import Users from './compnents/Users/index.jsx';
-import Posts from './compnents/Posts/index.jsx';
+import "./App.css";
+import { Suspense } from "react";
+import Users from "./compnents/Users/index.jsx";
 
 function App() {
-    const count = useSelector((state) => state.counter.value);
-    const dispatch = useDispatch();
-
-    return (
-        <>
-            <div style={{padding: 20}}>
-                <h1>Счётчик: {count}</h1>
-                <button onClick={() => dispatch(decrement())}>-</button>
-                <button onClick={() => dispatch(increment())}>+</button>
-            </div>
-            <Users/>
-            <Posts/>
-        </>
-    );
+  return (
+    <>
+      <Suspense fallback={<div>Загружаем...</div>}>
+        <Users />
+      </Suspense>
+    </>
+  );
 }
 
 export default App;
--- src/compnents/Users/index.jsx
+++ src/compnents/Users/index.jsx
@@ -1,60 +1,36 @@
-import {useDispatch, useSelector} from 'react-redux';
-import {useState} from 'react';
-import {addUser, removeUser} from '../../features/users/usersSlice.js';
+let userData = null;
+let promise = null;
 
-import './style.css';
+function loadUser() {
+  if (!promise) {
+    promise = fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => (userData = data));
+  }
+
+  return {
+    read() {
+      if (!userData) throw promise;
+      return userData;
+    },
+  };
+}
+
+const userResource = loadUser();
 
 const Users = () => {
-    const [newUser, setNewUser] = useState('');
-    const users = useSelector((state) => state.users.users);
-    const dispatch = useDispatch();
+  const user = userResource.read(); // Используем Suspense ресурс
 
-    const handleAddUser = (e) => {
-        e.preventDefault();
-        if (newUser.trim() === '') {
-            alert('введите имя');
-        } else {
-            dispatch(addUser(newUser));
-            setNewUser('');
-        }
-
-    };
-
-    const handleRemoveUser = (id) => {
-        dispatch(removeUser(id));
-    };
-
-
-    return (
-
-
-        <div className="container">
-            <h3>Users</h3>
-            <form className="form" onSubmit={handleAddUser}>
-                <input
-                    className="input"
-                    type="text"
-                    placeholder="введите имя"
-                    value={newUser}
-                    onChange={(e) => setNewUser(e.target.value)}
-                />
-                <button type="submit">Добавить</button>
-            </form>
-
-            <div>
-                <ul className="list">
-                    {users.map((user) => (
-                        <li className="list-item" key={user.id}>
-                            <div>{user.name}</div>
-                            <button onClick={() => handleRemoveUser(user.id)}>Удалить</button>
-                        </li>
-
-                    ))}
-
-                </ul>
-            </div>
+  return (
+    <div>
+      {user.map((item) => (
+        <div>
+          <h2>{item.name}</h2>
+          <p>Email: {item.email}</p>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default Users;

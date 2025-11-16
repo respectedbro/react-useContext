fb468b500a363e0da29c9e6c07dff84817bcf1e6
resAsus <respectedbro@yandex.ru>
2025-11-16 19:35:47 +0300

    with_modal

--- src/compnents/Modal/index.jsx
+++ src/compnents/Modal/index.jsx
@@ -1,20 +1,26 @@
 import './style.css';
 import {useState} from 'react';
+import {createPortal} from 'react-dom';
+
+const modalRoot = document.getElementById('portal');
 
 export const Modal = ({handleClose, text, handleSave}) => {
     const [value, setValue] = useState(text);
 
-    return (
-        <div className="modal">
-            <span onClick={handleClose}>X</span>
-            <h3>Edit text</h3>
-            <input
-                type="text"
-                value={value}
-                onChange={(e) => setValue(e.target.value)}
 
-            />
-            <button onClick={() => handleSave(value)}>Save</button>
-        </div>
+    return createPortal(
+        <div className='modal-overlay'>
+            <div className="modal">
+                <span onClick={handleClose}>X</span>
+                <h3>Edit text</h3>
+                <input
+                    type="text"
+                    value={value}
+                    onChange={(e) => setValue(e.target.value)}
+
+                />
+                <button onClick={() => handleSave(value)}>Save</button>
+            </div>
+        </div>, modalRoot
     );
 };

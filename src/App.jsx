db631016f679988c145afb7ae22f38c540df1ca7
NikDm <respectedoff@yandex.ru>
2025-11-17 12:03:19 +0300

    portal

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,36 @@
-import './App.css';
-import {useState} from 'react';
-import {Modal} from './compnents/Modal/index.jsx';
-
+import "./App.css";
+import { useState } from "react";
+import { Modal } from "./compnents/Modal/index.jsx";
 
 function App() {
-    const [text, setText] = useState('Some TExt');
-    const [openModal, setOpenModal] = useState(false);
+  const [text, setText] = useState("Some TExt");
+  const [openModal, setOpenModal] = useState(false);
 
-    const handleClose = () => {
-        setOpenModal(false);
-    };
+  const handleClose = () => {
+    setOpenModal(false);
+  };
 
-    const handleSave = (value) => {
-        setText(value);
-        setOpenModal(false);
-    };
+  const handleSave = (value) => {
+    setText(value);
+    setOpenModal(false);
+  };
 
-    return (
-        <div style={{ padding: 20,position: "relative" }}>
-            <h1> text: {text}</h1>
-            <button onClick={() => setOpenModal(true)}>Edit</button>
-            {
-                openModal && (
-                    <Modal
-                        handleClose={handleClose}
-                        handleSave={handleSave}
-                        text={text}
-                    />
-                )
-            }
-
-        </div>
-    );
+  return (
+    <div
+      style={{
+        padding: 20,
+        position: "relative",
+        height: "200vh",
+        overflow: "hidden",
+      }}
+    >
+      <h1> text: {text}</h1>
+      <button onClick={() => setOpenModal(true)}>Edit</button>
+      {openModal && (
+        <Modal handleClose={handleClose} handleSave={handleSave} text={text} />
+      )}
+    </div>
+  );
 }
 
 export default App;
